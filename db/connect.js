e8add20a9749c7859f18629ad971de7c027c4250
andre-wallan <andressemanda@gmail.com>
2026-03-16 11:56:41 +0300

    vmn

--- db/connect.js
+++ db/connect.js
@@ -6,17 +6,24 @@ const initDb = (callback) => {
   if (database) {
     return callback(null, database);
   }
+
   MongoClient.connect(process.env.MONGODB_URI)
     .then((client) => {
-      database = client.db();
+      // Explicitly select the DB name you are using in MongoDB
+      database = client.db('cse341'); // <- make sure this matches your DB
+      console.log('Connected to MongoDB');
       callback(null, database);
     })
     .catch((err) => {
+      console.error('Failed to connect to MongoDB', err);
       callback(err);
     });
 };
 
 const getDb = () => {
+  if (!database) {
+    throw new Error('Database not initialized');
+  }
   return database;
 };
 
